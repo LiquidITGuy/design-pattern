f52c2ce2e17a22e22a962cbbdf3fbb8cc0c4e47d
Simon Belbeoch <simon.belbeoch@ln1.eu>
2023-08-25 20:18:02 +0200

    feat(singleton): ajout du singleton compteur

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,18 @@
+import { Compteur } from './singleton/Compteur.class.ts'
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+  const setCounter = (valeur: number | void) => {
+    // Ici, on récupère l'instance à chaque fois pour vérifier qu'il s'agit bien d'une instance unique
+    const compteur = Compteur.getInstance()
+    if(valeur){
+      compteur.setValeur(valeur)
+    }
+    else {
+      compteur.add()
+    }
+    const compte = compteur.compte
+    element.innerHTML = `count is ${compte}`
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
+
+  element.addEventListener('click', () => setCounter())
   setCounter(0)
 }
